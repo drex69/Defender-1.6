a82f2bfb9571b37d032e1a1e09240dccd89cfc54
drex69 <73351169+drex69@users.noreply.github.com>
2021-12-12 13:10:11 +0000

    Update insert_coin_screen_class.js

--- screen/insert_coin_screen_class.js
+++ screen/insert_coin_screen_class.js
@@ -32,15 +32,7 @@ class insert_coin_screen_class
     center_text(this.text2);
     text(this.text2,text_x,450/ws);
     
-     if(touches.length == 1)
-    {
-      //start_screen.stop = frameCount + 250;
-      start_screen = new start_screen_class();
-      game_state = 0;
-      menu_screen_sound.play();
-      touches.length = 0;
-    }
-
+    
   }
 
   update()
